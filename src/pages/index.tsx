9f471196c80ad097d165ba82c745a68cc2fed7f7
Mkhs <jonbecher@microagenetworks.com>
2022-08-17 14:59:07 -0400

    Implemented a noPosts and hasPosts consts in Home function to perform the listing of the posts elements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,6 +19,27 @@ export async function getStaticProps() {
 }
 
 export default function Home({ posts }: { posts: Post[] }) {
+  
+  const noPosts = <p className="text-slate-700">Nothing posted yet.</p>
+  const hasPosts = (
+    <ul>
+      {posts.map(post => {
+        return (
+          <li key={post.title} className="flex justify-between mb-3">
+            <h2 className="text-lg text-slate-900 hover:text-blue-600">
+              <Link href={`/posts/${post.slug}`}>
+                <a>{post.title}</a>
+              </Link>
+            </h2>
+            <p className="min-w-[90px] text-right text-slate-500">
+              {new Date(post.date).toLocaleDateString('en-IN')}
+            </p>
+          </li>
+        );
+      })}
+    </ul>
+  )
+  
   return (
     <>
       <h1 className="text-2xl sm:text-3xl font-bold text-slate-900 tracking-tight mb-3">Hello there!</h1>
