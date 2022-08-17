8d78ea315f041e7f7ee574aa8de5130dd730feae
Mkhs <jonbecher@microagenetworks.com>
2022-08-17 17:12:14 -0400

    Updated Header

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
                 <p className="text-2xl font-extrabold">
                     <Link href="/">
                         <a title="Go to homepage" aria-label="Go to homepage">
-                            My Blog
+                            Jon's Blog
                         </a>
                     </Link>
                 </p>
