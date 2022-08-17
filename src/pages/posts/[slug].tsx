38d40fb7c5b8927a31fae7298c4e13cd5f319ddf
Mkhs <jonbecher@microagenetworks.com>
2022-08-17 15:33:29 -0400

    Added ability to use both md and mdx files, no longer using the dangerouslysethtml element

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 import { allPosts, type Post } from 'contentlayer/generated';
+import Image from 'next/image';
+import { useMDXComponent } from 'next-contentlayer/hooks'
 
 export async function getStaticProps({ params }: { params: { slug: string } }) {
     const post = allPosts.find(postFromAll => postFromAll.slug === params.slug);
@@ -24,21 +26,26 @@ export async function getStaticPaths() {
 }
 
 export default function Post({ post }: { post: Post }) {
+    const components = {
+      Image,
+    };
+    const MDXContent = useMDXComponent(post.body.code);
+  
     return (
-        <>
-            <Head>
-                <title>{post.title}</title>
-            </Head>
-            <article className='prose prose-slate'>
-                <h1 className="text-center mb-3">{post.title}</h1>
-                <p className='text-slate-500 text-center'>
-                    Posted on{' '}
-                    <time dateTime={post.date} title={new Date(post.date).toString()}>
-                        {new Date(post.date).toLocaleDateString('en-IN')}
-                    </time>
-                </p>
-                <div dangerouslySetInnerHTML={{ __html: post.body.html}} />
-            </article>
-        </>
+      <>
+        <Head>
+          <title>{post.title}</title>
+        </Head>
+        <article className="prose prose-slate lg:prose-xl">
+          <h1 className="text-center mb-3">{post.title}</h1>
+          <p className="text-slate-500 text-center">
+            Posted on{' '}
+            <time dateTime={post.date} title={new Date(post.date).toString()}>
+              {new Date(post.date).toLocaleDateString('en-CA')}
+            </time>
+          </p>
+          <MDXContent components={components} />
+        </article>
+      </>
     );
-}
+  }
