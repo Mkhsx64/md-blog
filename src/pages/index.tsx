f80f75c04131ce96de1f13ba5203b54d88b049e7
Mkhs <jonbecher@microagenetworks.com>
2022-08-17 14:53:36 -0400

    Added async function to sort posts on the homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,24 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { allPosts, Post } from 'contentlayer/generated';
+import Link from 'next/link';
 
-const Home: NextPage = () => {
+// Displays a list of pages on the homepage sorted by date/time
+export async function getStaticProps() {
+  const posts = allPosts.sort((a, b) => {
+    if (new Date(a.date).getTime() >= new Date(b.date).getTime()) {
+      return -1;
+    }
+
+    return 1;
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default function Home({ posts }: { posts: Post[] }) {
   return (
     <>
       <h1 className="text-2xl sm:text-3xl font-bold text-slate-900 tracking-tight mb-3">Hello there!</h1>
@@ -11,5 +26,3 @@ const Home: NextPage = () => {
     </>
   )
 }
-
-export default Home
